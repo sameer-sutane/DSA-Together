7f0a1e2f69f0aaf310fc4d7df8f6bce35d8e3b9c
sameer-sutane <99412965+sameer-sutane@users.noreply.github.com>
2022-11-29 20:10:05 +0530

    Add files via upload

---
+++ 001_Search_Element_In_Array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,ele,x,flag=0;
+    cout<<"Enter number of elements: ";
+    cin >>n;
+    int arr[n];
+    cout<<"Enter the elements\n";
+    for(i=0;i<n;i++)
+    {
+        cin>>ele;
+        arr[i]=ele;
+    }
+    cout<<"entered elements are: ";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<< "\nenter the element to search: ";
+    cin>>x;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            cout<<"element "<<x<<" found at index "<<i;
+            flag=1;
+        }
+        
+    }
+    if(flag==0)
+        {
+            cout << "element not present in given array";
+        }
+}
